098df436f0992b7231e88a8f36925e4a14037248
katrin.sooaar <katrin.sooaar@khk.ee>
2018-12-11 09:38:40 +0200

    Lisatud ülesanne 1

--- kontroll.php
+++ kontroll.php
@@ -5,6 +5,15 @@
  * Date: 11.12.2018
  * Time: 9:22
  */
+// Ülesanne 1
+/* Loo programm, mille sees:
+a) defineeritud muutujate abil
+    Sinu ees- ja perenimi
+    kursuse tähis
+    kursuse number
+    Sinu kooli email
+b) Väljastatud muutujate väärtused koos sobiva tekstiga html faili kujul
+*/
 $nimi = 'Katrin Sooäär';
 $kursuseTahis = 'KD';
 $kursuseNumber = 18;
