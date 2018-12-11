8934edc47a66c4042964d402a2abe5656aa9a180
katrin.sooaar <katrin.sooaar@khk.ee>
2018-12-11 08:41:00 +0200

    Added katse.php test file

---
+++ katse.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: katrin.sooaar
+ * Date: 06.12.2018
+ * Time: 11:08
+ */
+echo '
+    <!doctype html>
+    <html>
+        <head>
+            <title></title>
+        </head>
+        <body>
+            <h1>PHP abil loodud HTML</h1>
+        </body>
+    </html>
+';
+
+?>
