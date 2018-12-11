976b7d5194ffa1386e6409e60725fb3ebac4ae8a
katrin.sooaar <katrin.sooaar@khk.ee>
2018-12-11 09:21:19 +0200

    Lisatud kogu info muutujate kohta

--- katse.php
+++ katse.php
@@ -5,14 +5,49 @@
  * Date: 06.12.2018
  * Time: 11:08
  */
+// muutujate defineerimine
+// $muutujaNimi = väärtus;
+$lehePealkiri = 'Katse leht';
+$sisuPealkiri = 'Muutujate defineerimine';
+//muutujate sisu testvaatamine
+var_dump($lehePealkiri);
+echo '<br>';
+var_dump($sisuPealkiri);
+// andmetüübid
+$taisarv = 7;
+$reaalarv = -2.6;
+$tekst = 'Tere PHP';
+$toevaartusTrue = true;
+$toevaartusFalse = false;
+// väärtuse tüübi kontroll - gettype($muutujaNimi)
+// väärtuse tüübi teisendus - settype($muutujaNimi, 'tüüp')
+// väljastamine
+// ühendamine 'tekst '.$muutujaNimi.' tekst'
 echo '
     <!doctype html>
     <html>
         <head>
-            <title></title>
+            <title>'.$lehePealkiri.'</title>
         </head>
         <body>
-            <h1>PHP abil loodud HTML</h1>
+            <h1>'.$sisuPealkiri.'</h1>
+            <h3>Andmetüübid</h3>
+            <ul>
+                <li>$taisarv = '.$taisarv.' - '.gettype($taisarv).'</li>
+                <li>$reaalarv = '.$reaalarv.'  - '.gettype($reaalarv).'</li>
+                <li>$tekst = '.$tekst.'  - '.gettype($tekst).'</li>
+                <li>$toevaartusTrue = '.$toevaartusTrue.'  - '.gettype($toevaartusTrue).'</li>
+                <li>$toevaartusFalse = '.$toevaartusFalse.'  - '.gettype($toevaartusFalse).'</li>
+            </ul>
+            ';
+// tüübi teisendamine
+echo $taisarv.' + '.$taisarv.' = '.($taisarv + $taisarv);
+echo '<br>';
+settype($taisarv, 'string');
+echo $taisarv.' + '.$taisarv.' = '.($taisarv + $taisarv);
+echo '<br>';
+echo gettype($taisarv);
+echo '
         </body>
     </html>
 ';
