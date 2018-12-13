1f1360ffebd1908dceb18012de84b09361e12e2c
katrin.sooaar <katrin.sooaar@khk.ee>
2018-12-13 10:58:18 +0200

    Lisatud testväljastus

--- tabelTest.php
+++ tabelTest.php
@@ -9,9 +9,13 @@
 require_once './Tabel.php';
 // loome tabeli
 $lihtTabel = new Tabel();
+var_dump($lihtTabel);
 // lisame read sisuga
 $lihtTabel->lisaRida(array(1,2,3));
 $lihtTabel->lisaRida(array(4,5,6));
 $lihtTabel->lisaRida(array(7,8,9));
+echo '<pre>';
+print_r($lihtTabel);
+echo '</pre>';
 // trükin tabeli välja
 $lihtTabel->naitaTabel();
