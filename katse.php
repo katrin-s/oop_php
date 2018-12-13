2f0c985b101ea7acc342b7753445dca174d5761b
katrin.sooaar <katrin.sooaar@khk.ee>
2018-12-13 08:52:29 +0200

    Lisatud tabeli funktsioon

--- katse.php
+++ katse.php
@@ -6,27 +6,34 @@
  * Time: 11:08
  */
 // Tsüklid
-echo '<!doctype html><html><head><title>Tsüklid</title>
-<link rel="stylesheet" type="text/css" href="katsestyle.css"></head><body>';
-// tsüklid
-// for
+
+// funktsioonid
+// function
+// parameetrid pole kohustuslikud
 /*
- for($juhtimisM=algväärtus; $juhtimisM < lõppväärtus; $juhtimisM++){
-    tegevused, mis toimuvad nii kaua, kui $juhtimisM < lõppväärtus kehtib
+function funktsiooniNimi([$parameeter1,$parameeter2,...,$parameeterN]) {
+    kõik tegevused, mis peavad toimuma
 }
  */
-echo '<table>';
-$ridadeArv = 6;
-$veergudeArv = 5;
-for($reaNumber = 1; $reaNumber <= $ridadeArv; $reaNumber++){
-    echo '<tr>';
-    for ($veeruNumber = 1; $veeruNumber <= $veergudeArv; $veeruNumber++) {
-        echo '<td>';
-        echo $veeruNumber;
-        echo '</td>';
+function htmlTabel(){
+    echo '<table>';
+    $ridadeArv = 6;
+    $veergudeArv = 5;
+    for($reaNumber = 1; $reaNumber <= $ridadeArv; $reaNumber++){
+     echo '<tr>';
+     for ($veeruNumber = 1; $veeruNumber <= $veergudeArv; $veeruNumber++) {
+         echo '<td>';
+         echo $veeruNumber;
+         echo '</td>';
+     }
+     echo '</tr>';
     }
-    echo '</tr>';
+    echo '</table>';
 }
-echo '</table>';
+// lehe sisu väljastamine
+echo '<!doctype html><html><head><title>Funktsioonid</title>
+<link rel="stylesheet" type="text/css" href="katsestyle.css"></head><body>';
+// funktsiooni väljakutsumine
+htmlTabel();
 echo '</body></html>';
 ?>
