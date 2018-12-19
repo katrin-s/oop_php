4a128cf62a1a5fc430f507c6c3e14629be4b9df9
katrin.sooaar <katrin.sooaar@khk.ee>
2018-12-19 11:17:18 +0200

    Lisatud sisselogimise tervitus

--- sessioon/login_do.php
+++ sessioon/login_do.php
@@ -12,11 +12,15 @@ else if (empty($_POST['pass'])) {
     header('Location: login.php');
     $_SESSION['viga'] = 'Sisesta parool!';
 } else {
+    session_destroy(); //lõpetan veateate sessiooni
+    session_start(); // alustan sisselogitud kasutaja sessiooni
+    session_regenerate_id(); //määrame uue sessiooni ID
     //kontrollin sessiooni IDd
     echo session_id();
-    echo '<br>';
+    $_SESSION['username'] = $_POST['username'];
     // sessiooni töö pikkus on vaikimisi 30 minutit
     echo '<pre>';
     print_r($_SESSION);
     echo '</pre>';
+    echo 'Tere tulemast, '.$_SESSION['username'].'!';
 }
